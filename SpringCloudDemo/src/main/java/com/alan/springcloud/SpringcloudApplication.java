0dd321de42ba0d91d621bae676566d1cbee0b72e
cmr <1678336204@qq.com>
2021-06-11 22:30:46 +0800

    1111

--- SpringCloudDemo/src/main/java/com/alan/springcloud/SpringcloudApplication.java
+++ SpringCloudDemo/src/main/java/com/alan/springcloud/SpringcloudApplication.java
@@ -2,8 +2,10 @@ package com.alan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringcloudApplication {
 
     public static void main(String[] args) {
