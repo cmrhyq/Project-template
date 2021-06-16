af2f7a2e04724ae8e3b003f2cf33ad733bd50350
cmr <1678336204@qq.com>
2021-06-16 18:09:47 +0800

    frame

--- eureka_server/src/main/java/com/alan/eurekaProject/EurekaApplication.java
+++ eureka_server/src/main/java/com/alan/eurekaProject/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.alan.eurekaProject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -11,12 +12,16 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @date 2021/6/15-15:23
  * @Email cmrhyq@163.com
  */
+@Slf4j
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
+        long beginTime = System.currentTimeMillis();
         SpringApplication.run(EurekaApplication.class, args);
+        long endTime = System.currentTimeMillis();
+        log.info("程序启动耗时：" + (endTime - beginTime) + "ms");
     }
 
 }
