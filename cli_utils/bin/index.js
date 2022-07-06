cf409cc7b6ccbdc8d4a53276edb4efef4a8ef935
i拥玥 <1678336204@qq.com>
2022-07-07 00:59:47 +0800

    update factory project and documentation

---
+++ cli_utils/bin/index.js
@@ -0,0 +1,11 @@
+/**
+ * <p></p>
+ * 
+ * @author Alan Huang
+ * @version v1.0.0
+ * @className index.js.js
+ * @project cli_utils
+ * @package 
+ * @date 2022/7/7-0:26
+ * @email cmrhyq@163.com
+ */
