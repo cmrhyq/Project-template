92503a1dbf10410ba463ccce6aa0eb082d4af6fd
i拥玥 <1678336204@qq.com>
2022-07-06 10:17:20 +0800

    new design pattern study

---
+++ Design Patterns/Factory Pattern/factoryPattern/src/main/java/com/cmrhyq/design/pattern/service/IFruit.java
@@ -0,0 +1,13 @@
+package com.cmrhyq.design.pattern.service;/**
+ * <p></p>
+ * 
+ * @author Alan Huang
+ * @version v0.0.1
+ * @since v0.0.1
+ * @project pattern
+ * @docRoot com.cmrhyq.design.pattern.service.IFruit
+ * @date 2022/7/6-1:00
+ * @email cmrhyq@163.com
+ */
+public interface IFruit {
+}
