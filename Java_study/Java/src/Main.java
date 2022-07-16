cb765406d5602bec5fc672943c773bfee8837cdf
i拥玥 <1678336204@qq.com>
2022-07-16 20:41:58 +0800

    java study project

---
+++ Java_study/Java/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+//        int z = 3 >> 2;
+//        System.out.println("z = " + z);
+
+        // ++a 是单目运算：单目运算是只对一个值进行运算
+        // 1级 (){}
+        // 2级 单目运算
+        // 3级 算术运算符
+        // 4级 位移运算符
+        // 5级 比较运算符
+        // 6级 逻辑运算符
+        // 7级 三元运算符
+        // 8级 赋值运算符
+    }
+}
