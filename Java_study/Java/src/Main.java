3996e54bb8a3ce61616a6c8ab106f0bcfb6d6cef
i拥玥 <1678336204@qq.com>
2022-07-19 02:00:29 +0800

    2022/7/19 2:00

--- Java_study/Java/src/Main.java
+++ Java_study/Java/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        int z = 3 >> 2;
@@ -12,5 +15,26 @@ public class Main {
         // 6级 逻辑运算符
         // 7级 三元运算符
         // 8级 赋值运算符
+
+//        int n1 = 0x23A;
+//        System.out.println("n1 = " + n1);
+
+//        int a = 1 >> 2; // 1向右位移2位
+//        int b = -1 >> 2;
+//        int c = 1 << 2;
+//        int d = -1 << 2;
+//        int e = 3 >>> 2;
+//        System.out.println("a=" + a);
+//        System.out.println("b=" + b);
+//        System.out.println("c=" + c);
+//        System.out.println("d=" + d);
+//        System.out.println("e=" + e);
+
+//        Scanner scanner = new Scanner(System.in);
+//        double score = scanner.nextDouble();
+//        char sex = scanner.next().charAt(0);
+//        System.out.println("score :" + score + ", sex :" + sex);
+
+
     }
 }
