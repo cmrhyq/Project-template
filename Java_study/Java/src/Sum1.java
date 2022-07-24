49d6cc6cdddada416de1d5c3235a7e530029bab1
i拥玥 <1678336204@qq.com>
2022-07-25 01:09:16 +0800

    输出1-1/2+1/3-1/4....1/100的和

--- Java_study/Java/src/Sum1.java
+++ Java_study/Java/src/Sum1.java
@@ -13,9 +13,25 @@ public class Sum1 {
 
     /**
      * 输出1-1/2+1/3-1/4....1/100的和
+     *
      * @param args
      */
     public static void main(String[] args) {
-
+        int[] interval = {2, 100};
+        double sum = 0;
+        for (int i = interval[0]; i <= interval[1]; i++) {
+            if (i == 2) {
+                sum = 1.0 - (1.0 / i);
+            } else {
+                if ((i - 1) % 2 != 0) {
+                    sum = sum - (1.0 / i);
+                } else if ((i - 1) % 2 == 0){
+                    sum = sum + (1.0 / i);
+                } else {
+                    System.out.println("error: application error");
+                }
+            }
+        }
+        System.out.println("1-1/2+1/3-1/4....1/100 = " + sum);
     }
 }
