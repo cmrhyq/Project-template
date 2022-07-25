6c8950a4fab2b53ce2cefb75f8289f29f80882ab
i拥玥 <1678336204@qq.com>
2022-07-26 00:26:12 +0800

    求1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+4+...+100)的结果

---
+++ Java_study/Java/src/Sum2.java
@@ -0,0 +1,13 @@
+package PACKAGE_NAME;/**
+ * <p></p>
+ * 
+ * @author Alan Huang
+ * @version v0.0.1
+ * @since v0.0.1
+ * @project Java
+ * @docRoot PACKAGE_NAME.sum2
+ * @date 2022/7/25-1:09
+ * @email cmrhyq@163.com
+ */
+public class sum2 {
+}
