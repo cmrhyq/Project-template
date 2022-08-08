a80b550c67b1297f49aaa43efefb0467f9ed4ac0
i拥玥 <1678336204@qq.com>
2022-08-08 23:01:42 +0800

    automation a login function and

--- ACAM_Automation/main.py
+++ ACAM_Automation/main.py
@@ -1,12 +1,44 @@
+import logging
+import time
+
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 from config.read_yaml import read_yaml_content
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 
 config = read_yaml_content()
-path = Service(config['webInterface']['edge']['path'])
-browser = webdriver.Edge(service=path)
-try:
-    browser.get("http://10.1.208.228:9991/acam/?ticket=ST-1885-Lod9wj8ZD18QyoNmkjLi&host=10.1.208.228:9991#/iframe"
-                "/25020016?_r=1658886884662")
-finally:
-    browser.close()
+path = Service(config['webInterface']['chrome']['path'])
+browser = webdriver.Chrome(service=path)
+logging.basicConfig(level=config['log']['level'],
+                    filename=config['log']['filename'],
+                    filemode='w',
+                    format=config['log']['format'])
+
+
+def login():
+    try:
+        browser.get(config['webInterface']['url'])
+        logging.info('login url is:' + config['webInterface']['url'])
+        account_elem = browser.find_element(By.ID, 'username')
+        account_elem.send_keys(config['safety']['account'])
+        logging.info('input account:' + config['safety']['account'])
+        pwd_elem = browser.find_element(By.ID, 'password')
+        pwd_elem.send_keys(config['safety']['password'])
+        browser.find_element(By.ID, 'loginButton').click()
+        time.sleep(3)
+    except Exception as err:
+        print(err)
+
+
+def to_app_manage():
+    manage_btn = browser.find_element(By.CSS_SELECTOR, 'span.icon.iconfont.icon-mYyddgl')
+    ActionChains(browser).move_to_element(manage_btn).perform()
+    app_manage_btn = browser.find_element(By.XPATH, '//li[@data-url="/#/apply-configure-manage/apply-manage"]')
+    app_manage_btn.click()
+
+
+if __name__ == '__main__':
+    login()
+    to_app_manage()
--- ACAM_Automation/util/__init__.py
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env python
-# Author: AlanHuang
-# Time: 2022/8/7 13:47
-# Description: 
-
-
-if __name__ == '__main__':
-    print_hi('Python')
